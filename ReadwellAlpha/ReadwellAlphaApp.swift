6869220ccac29214e3799d4154eb8ffdefea35f4
AnanyaGrover1 <ananyag@princeton.edu>
2022-01-03 18:48:00 +0530

    Initial Commit

---
+++ ReadwellAlpha/ReadwellAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadwellAlphaApp.swift
+//  ReadwellAlpha
+//
+//  Created by Ananya Grover on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadwellAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
